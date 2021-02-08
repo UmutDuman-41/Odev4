a2d914f90509e74f1c1460a072602ce014aa5c02
Umut Duman <uduman0629@gmail.com>
2021-02-08 23:35:20 +0300

    Add project files.

---
+++ Odev4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Odev4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(3, "Alper");
+            myDictionary.Add(34, "Nevzat");
+            myDictionary.Add(26, "Kaan");
+
+            myDictionary.List();
+
+        }
+    }
+}
